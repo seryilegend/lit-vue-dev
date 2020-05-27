3cd9271524eb3da6dec2cac059476ed5cf11a4ef
legenda <kors8705@gmail.com>
2020-05-27 18:36:13 +0300

    Test

--- src/main.js
+++ src/main.js
@@ -5,23 +5,28 @@ import example_1 from './components/example_1.vue'
 import example_2 from './components/example_2.vue'
 import App from './App.vue'
 import VueCycle from './components/VueCycle.vue'
+import { APP_STORE } from './components/store'
 
 Vue.config.productionTip = false
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const routes = [
   { path: '/', component: Header },
   { path: '/example_1/', component: example_1 },
   { path: '/example_2/', component: example_2 },
   { path: '/vuecycle/', component: VueCycle }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   routes
-})
+});
+
+const store = new Vuex.Store(APP_STORE);
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
