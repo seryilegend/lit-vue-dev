13a72e199bfbba7411c19d130595bdeb9d733503
legenda <kors8705@gmail.com>
2020-05-27 19:10:46 +0300

    Test

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -12,7 +12,7 @@ export const APP_STORE = {
         },
     },
     actions: {
-        increment_count({ commit }) {
+        incrementCount({ commit }) {
             commit('INCREMENT_COUNT');
         },
         changeUserPhrase({ commit }, value) {
