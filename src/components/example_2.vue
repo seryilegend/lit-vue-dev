a97fbb09e11e493de7f37875a5aa085d4531f542
seryilegend <63164708+seryilegend@users.noreply.github.com>
2020-11-11 21:39:22 +0300

    props

--- src/components/example_2.vue
+++ src/components/example_2.vue
@@ -1,25 +1,42 @@
 <template>
-    <div>
-        <div class='a'>→Everything is temporary←</div>
-        <div class="b"><router-link to='/'>Header</router-link></div>
-        <img src="https://clippz.ru/images/imagine-dragons-birds_w350r42f/50.jpg">
+    <div class="wrapper">
+        <router-link to='/'><button class="link">Header</button></router-link>
+        <buttonComponent :text="vis ? 'Скрыть список' : 'Показать список'" :theme="vis ? 'primary' : 'regular'" :onClick="() => vis = !vis" />
+        <listComponent v-if="vis" :param="objects" />
     </div>
 </template>
 
 <script>
+import buttonComponent from './buttonComponent'
+import listComponent from './listComponent'
 export default {
-    name: 'example_2'
+    name: 'example_2',
+    components: {buttonComponent, listComponent},
+    data() {
+        return {
+            vis: true,
+            objects: [
+                {title: 'Big thangs', description: 'sure enough'},
+                {title: 'Theres a monster', description: 'coming from the forest'},
+                {title: 'Sheep', description: 'gang glorious'},
+                {title: 'Run thru the city', description: 'with a message for it'}
+            ]
+        }
+    }
 }
 </script>
 
 <style scoped>
-    .a {
-        text-align: center;
-        color: midnightblue;
-        background-color: pink;
+    .wrapper {
+        margin-top: 100px;
     }
-    .b {
-        margin-top: 10px;
-        background-color: aqua;
+    .link {
+        margin-top: 1rem;
+        border-radius: 1rem;
+        cursor: pointer;
+        margin: 10px;
+        height: 2rem;
+        width: 10rem;
+        color: blue;
     }
 </style>
---
+++ src/components/listComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="wrapper">
+        <table>
+            <tr>
+                <td>Title</td>
+                <td>Description</td>
+            </tr>
+            <tr v-for="item in param" v-bind:key="[item.title, item.description]">
+                <td>{{ item.title }}</td>
+                <td>{{ item.description }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'listComponent',
+    props: {
+        param: Array
+    }
+}
+</script>
+
+<style scoped>
+    .wrapper {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+    table {
+        border-collapse: collapse;
+    }
+    th {
+        padding: 5px;
+        border: 1px solid black;
+    }
+    td {
+        padding: 5px;
+        border: 1px solid black;
+    }
+</style>
