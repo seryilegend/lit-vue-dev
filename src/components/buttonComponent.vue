3e9f7fe67b1d4f64019829c0d8854673d5e37313
seryilegend <63164708+seryilegend@users.noreply.github.com>
2020-11-11 21:39:22 +0300

    props

---
+++ src/components/buttonComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button :class="[theme, 'button']" @click="onClick">{{ text }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'buttonComponent',
+    props: {
+        theme: String,
+        text: String,
+        onClick: Function
+    }
+}
+</script>
+
+<style scoped>
+    .button {
+        border-radius: 1rem;
+        cursor: pointer;
+        margin: 10px;
+        height: 2rem;
+        width: 10rem;
+    }
+    .primary {
+        background-color: red;
+    }
+    .regular {
+        background-color: aqua;
+    }
+</style>
